2ff98e1dcd01a2bb1f6f1b2f34445d01d9ea5f3e
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2024-08-23 19:36:03 -0400

    solved: leetcode 88

--- two_pointers/merge_sorted_array.py
+++ two_pointers/merge_sorted_array.py
@@ -2,17 +2,26 @@ from typing import List
 
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-    i, j, k = m - 1, n - 1, (m + n) - 1
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
 
-    while j >= 0:
-        if i >= 0 and nums1[i] > nums2[j]:
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
             nums1[k] = nums1[i]
             i -= 1
+
         else:
             nums1[k] = nums2[j]
             j -= 1
         k -= 1
-    print(nums1)
+
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
+
+    return nums1
 
 
 print(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
