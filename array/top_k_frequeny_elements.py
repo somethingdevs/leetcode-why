182343e56662bed5be3c59bb275d6740d76b4c5d
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2024-08-24 22:45:19 -0400

    solved: leetcode 347

---
+++ array/top_k_frequeny_elements.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import defaultdict
+
+
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+
+nums = [10, 10, 10, 20, 20, 30]
+print(topKFrequent(nums=nums, k=2))
