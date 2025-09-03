f3255e0bb46666f6fc96df6b5183dc66b5e35651
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2025-09-02 23:22:18 -0400

    solved: leetcode 217

--- array/contains_duplicate.py
+++ array/contains_duplicate.py
@@ -12,4 +12,13 @@ def containsDuplicate(nums: List[int]) -> bool:
     return False
 
 
+# Solution using sorting
+def containsDuplicate(nums: List[int]) -> bool:
+    nums = sorted(nums)
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return True
+    return False
+
+
 print(containsDuplicate([1, 2, 3, 1]))
