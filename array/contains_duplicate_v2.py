88b9caeeb773199896b195baee46d871d33dd4d0
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2025-12-02 07:23:22 -0600

    resolved: leetcode 217

---
+++ array/contains_duplicate_v2.py
@@ -0,0 +1,30 @@
+# Using sort, O(n) - if you don't consider the sort
+from typing import List
+
+
+def containsDuplicate(nums: List[int]) -> bool:
+    nums = sorted(nums)
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
+
+# Using hashmap, O(n)
+def containsDuplicate(nums: List[int]) -> bool:
+    seen = {}
+    for index, value in enumerate(nums):
+        if value in seen:
+            return True
+        seen[value] = True
+    return False
+
+# Using set, O(n)
+def containsDuplicate(nums: List[int]) -> bool:
+    seen = set()
+    for index, value in enumerate(nums):
+        if value in seen:
+            return True
+        seen.add(value)
+    return False
+
+print(containsDuplicate([1, 2, 3, 1]))
